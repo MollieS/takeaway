298e26c869d3b5f45376aedabca47a800a42dff1
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-08-13 11:11:00 +0100

    customer can view menu

---
+++ lib/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant
+  def view_menu
+    { pizza: "£10.00",
+      beer: "£7.00" }
+  end
+end
--- spec/features/takeaway_feature_spec.rb
+++ spec/features/takeaway_feature_spec.rb
@@ -1,6 +1,8 @@
+require 'restaurant'
+
 feature 'Ordering a takeaway' do
   scenario 'viewing the menu' do
     restaurant = Restaurant.new
-    expect(restaurant.view_menu).to eq { item1: "£10.00", item2: "$7.00" }
+    expect(restaurant.view_menu).to eq({ pizza: "£10.00", beer: "£7.00" })
   end
 end
